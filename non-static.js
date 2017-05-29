fce448be228ce5be6efbe6bd298b8f6ecfa8d4fc
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-05-29 12:05:33 +0800

    Add static examples

--- non-static.js
+++
@@ -1,6 +0,0 @@
-function addition (x, y) {
-  return x + y
-}
-
-console.log(addition(1, 2))
-console.log(addition(true, false))
---
+++ static/non-static.js
@@ -0,0 +1,6 @@
+function addition (x, y) {
+  return x + y
+}
+
+console.log(addition(1, 2)) // returns 3
+console.log(addition(true, false)) // ?
