eb426d9c419235862ee437a05709de13cd70a580
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-06-01 09:58:16 +0800

    Update slides

--- concurrency/main.go
+++ concurrency/main.go
@@ -10,11 +10,13 @@ func main() {
 	defer timeTrack(time.Now(), "delay test")
 	c := make(chan bool, 2)
 	go func() {
+		// Delay by one second
 		time.Sleep(time.Second)
 		c <- true
 	}()
 
 	go func() {
+		// Delay by two seconds
 		time.Sleep(2 * time.Second)
 		c <- true
 	}()
